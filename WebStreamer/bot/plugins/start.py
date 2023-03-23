70bb38f8b5ac7d3abe3d281c3320a636d9323595
marduk <z.i.q665@gmail.com>
2023-03-24 00:52:08 +0300

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -6,12 +6,6 @@ from pyrogram.types import Message
 
 from WebStreamer.vars import Var 
 from WebStreamer.bot import StreamBot
-from pyrogram import filters, Client
-from pyrogram.errors import UserNotParticipant
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from WebStreamer.utils.database import Database
-db = Database(Var.DATABASE_URL, Var.name)
-
 
 @StreamBot.on_message(filters.command(["start", "help"]) & filters.private)
 async def start(_, m: Message):
@@ -23,28 +17,4 @@ async def start(_, m: Message):
         )
     await m.reply(
         f'مرحبا {m.from_user.mention(style="md")},ارسل الي ملف لاقوم بتوليد رابط تشغيل مباشر  , تابع طريقة المشاهدة هنا  @l_l_U .')
-    
-    ),
-    
-    if Var.UPDATES_CHANNEL != "None":
-        try:
-            user = await Client.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
-            if user.status == "kicked":
-                await Client.send_message(
-                    chat_id=m.chat.id,
-                    text="You are banned!\n\n  **Cᴏɴᴛᴀᴄᴛ Dᴇᴠᴇʟᴏᴘᴇʀ [MARDUK](https://t.me/AKKK7) ʜᴇ Wɪʟʟ Hᴇʟᴘ Yᴏᴜ**",
-                    
-                    disable_web_page_preview=True
-                )
-                return 
-        except UserNotParticipant:
-            await Client.send_message(
-                chat_id=m.chat.id,
-                text="""<i>🔐انظم الى القناة ليعمل البوت🔐</i>""",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("انظم الان 🔓", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
-                        ]
-                    ]
-                )
+    )
