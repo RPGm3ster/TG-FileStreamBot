9a618c8da58bbd1267c61fa847a4c259222c539f
marduk <z.i.q665@gmail.com>
2023-04-18 22:43:35 +0300

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -1,14 +1,19 @@
 from pyrogram import filters
 from pyrogram.types import Message
-
 from WebStreamer.vars import Var 
 from WebStreamer.bot import StreamBot
 
 @StreamBot.on_message(filters.command(["start", "help"]) & filters.private)
 async def start(_, m: Message):
-    if not await check_user_membership(m.from_user.id):
-        await StreamBot.send_message(chat_id=m.chat.id, text=f"الرجاء الاشتراك في القناة {https://t.me/kekkkkk} لاستخدام البوت.", disable_web_page_preview=True)
-        return
+    if Var.UPDATES_CHANNEL:
+        try:
+            chat = await StreamBot.get_chat_member(Var.UPDATES_CHANNEL, m.from_user.id)
+            if chat.status == "kicked":
+                return await m.reply("عذراً، لا يمكنك استخدام هذا البوت. إذا كنت تريد الاستمرار في استخدامه، يرجى الاتصال بمشرف.")
+        except UserNotParticipant:
+            return await m.reply(f"**يرجى الانضمام إلى قناة التحديثات الخاصة بنا أولاً:**\n\n{Var.UPDATES_CHANNEL}\n\nبعد الانضمام إلى القناة، يمكنك استخدام هذا البوت.", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("أضغط هنا للانضمام", url=f"t.me/{Var.UPDATES_CHANNEL}")]]))
+        except Exception:
+            return await m.reply("عذرًا، حدث خطأ غير معروف. يرجى إعادة المحاولة في وقت لاحق.")
     if Var.ALLOWED_USERS and not ((str(m.from_user.id) in Var.ALLOWED_USERS) or (m.from_user.username in Var.ALLOWED_USERS)):
         return await m.reply(
             "You are not in the allowed list of users who can use me. \
@@ -18,8 +23,3 @@ async def start(_, m: Message):
     await m.reply(
         f'مرحبا {m.from_user.mention(style="md")},ارسل الي ملف لاقوم بتوليد رابط تشغيل مباشر  , تابع طريقة المشاهدة هنا  @l_l_U .'
     )
-
-async def check_user_membership(user_id: int) -> bool:
-    chat = await StreamBot.get_chat(chat_id=-1001420087793)
-    member = await StreamBot.get_chat_member(chat_id=chat.id, user_id=user_id)
-    return member.status in ("creator", "administrator", "member")
